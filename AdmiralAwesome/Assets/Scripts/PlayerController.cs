6402c8d621729c92f3f53e62d5b7bc994a208eae
Remlik <Remlikmg@comcast.net>
2018-07-02 17:00:31 -0400

    Player can regen health

--- AdmiralAwesome/Assets/Scripts/PlayerController.cs
+++ AdmiralAwesome/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,12 @@ public class PlayerController : MonoBehaviour {
     public int health;
     public int maxHealth;
     public bool immortal;
+    public float regenRate;
+    public float regenLockout;
+    public bool regening;
 
+    private float nextRegenTime;
+    private float regenStartTime;
     private GameController game;
 
 	// Use this for initialization
@@ -18,13 +23,41 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (!regening)
+        {
+            if (Time.time >= regenStartTime)
+            {
+                regening = true;
+            }
+        } else
+        {
+            if (Time.time >= nextRegenTime)
+            {
+                AddHP(1);
+                nextRegenTime = Time.time + 1f / regenRate;
+            }
+        }
+    }
 
+    public void AddHP(int amount)
+    {
+        health += amount;
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+        if (game == null)
+        {
+            game = GameController._sharedInstance;
+        }
+        game.UpdateHealth((float)(health) / (float)(maxHealth));
     }
 
     public void Damage(int amount)
     {
         if (!immortal)
         {
+            regening = false;
             if (game == null)
             {
                 game = GameController._sharedInstance;
@@ -36,6 +69,7 @@ public class PlayerController : MonoBehaviour {
                 Die();
             }
             game.UpdateHealth((float)(health) / (float)(maxHealth));
+            regenStartTime = Time.time + regenLockout;
         }
     }
 
