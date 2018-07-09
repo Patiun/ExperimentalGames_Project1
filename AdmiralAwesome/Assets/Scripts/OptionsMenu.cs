ca6cb12df0d60721b989337a54946c569b3270cf
Remlik <Remlikmg@comcast.net>
2018-07-09 14:28:56 -0400

    Added preliminary credits and volume sliders for master volume, music volume, and effects volume

---
+++ AdmiralAwesome/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("masterVolume", volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("musicVolume", volume);
+    }
+
+    public void SetEffectVolume(float volume)
+    {
+        audioMixer.SetFloat("effectVolume", volume);
+    }
+}
