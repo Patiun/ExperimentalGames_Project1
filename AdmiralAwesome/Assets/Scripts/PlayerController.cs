8b4647f86ccd04033a92b8c889a0b7e118bcb0c3
Remlik <Remlikmg@comcast.net>
2018-07-09 12:58:58 -0400

    Removed an annoying Debug statement

--- AdmiralAwesome/Assets/Scripts/PlayerController.cs
+++ AdmiralAwesome/Assets/Scripts/PlayerController.cs
@@ -95,7 +95,6 @@ public class PlayerController : MonoBehaviour {
 
     public void SpeedBoost()
     {
-        Debug.Log("!!!!!!");
         boosted = true;
         fpc.m_WalkSpeed = boostSpeed;
         boostOff = Time.time + boostDuration;
