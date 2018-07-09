56c49ef6dd8c4a6b7c8f33c0e75295420aeb4e20
Remlik <Remlikmg@comcast.net>
2018-07-09 15:10:48 -0400

    Moved music to its own folder and implemented the lawnmower (does not spawn yet)

---
+++ AdmiralAwesome/Assets/Scripts/LawnMower.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LawnMower : AbstractEnemy, iSquashable, IPooledObject {
+
+    private ObjectPooler pooler;
+    public List<GameObject> enemies;
+
+	// Use this for initialization
+	void Start () {
+        Init();
+        
+	}
+	
+	// Update is called once per frame
+	new void Update () {
+        base.Update();
+	}
+
+    public bool CanSquash()
+    {
+        SquishFinder sf = SquishFinder._sharedInstance;
+        //Debug.Log((sf.CheckPoint(p1) + "," + sf.CheckPoint(p2) + "," + sf.CheckPoint(p3) + "," + sf.CheckPoint(p4)));
+        return (sf.CheckPoint(p1) && sf.CheckPoint(p2) && sf.CheckPoint(p3) && sf.CheckPoint(p4));
+    }
+
+    public void Squash()
+    {
+        pooler.SpawnFromPool("Explosion", transform.position + transform.forward, Quaternion.identity);
+        Detonate();
+        gameObject.SetActive(false);
+    }
+
+    public void Detonate()
+    {
+        foreach(GameObject enemy in enemies)
+        {
+            iSquashable s = enemy.GetComponent<iSquashable>();
+            if (s != null)
+            {
+                s.Squash();
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            enemies.Add(other.gameObject);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            enemies.Add(other.gameObject);
+        }
+    }
+
+    public void OnObjectSpawn()
+    {
+        Init();
+    }
+
+    public new void Init()
+    {
+        base.Init();
+        canDropPowerup = false;
+        if (pooler == null) { pooler = ObjectPooler._sharedInstance; }
+        enemies = new List<GameObject>();
+    }
+}
